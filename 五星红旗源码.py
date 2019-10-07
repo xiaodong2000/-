7508e9fef74855ec298de2c8996f581177c5de2e
xiaodong2000 <55126179+xiaodong2000@users.noreply.github.com>
2019-10-07 12:49:16 +0800

    Add files via upload

---
+++ 五星红旗源码.py
@@ -0,0 +1,68 @@
+import turtle as t
+
+#参数设置
+t.setup(600,400,0,0)
+t.bgcolor("red")
+t.fillcolor("yellow")
+t.color('yellow')
+t.speed(10)
+#主星
+t.begin_fill()
+t.up()
+t.goto(-600,220)
+t.down()
+for i in range (5):
+    t.forward(150)
+    t.right(144)
+t.end_fill()
+
+#第1颗副星
+t.begin_fill()
+t.up()
+t.goto(-400,295)
+t.setheading(305)
+t.down()
+for i in range (5):
+    t.forward(50)
+    t.left(144)
+
+t.end_fill()
+
+
+#第2颗副星
+t.begin_fill()
+t.up()
+t.goto(-350,212)
+t.setheading(30)
+t.down()
+for i in range (5):
+    t.forward(50)
+    t.right(144)
+
+t.end_fill()
+
+#第3颗副星
+t.begin_fill()
+t.up()
+t.goto(-350,145)
+t.setheading(5)
+t.down()
+for i in range (5):
+    t.forward(50)
+    t.right(144)
+
+t.end_fill()
+
+#第4颗副星
+t.begin_fill()
+t.up()
+t.goto(-400,90)
+t.setheading(300)
+t.down()
+for i in range (5):
+    t.forward(50)
+    t.left(144)
+
+t.end_fill()
+
+t.done()
